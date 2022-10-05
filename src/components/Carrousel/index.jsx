ff9d5605be53fd68c9290a66947c8646b3aca0fd
Louis-Étienne Girard <contact@louisetiennegirard.fr>
2022-10-05 08:35:22 +0200

    Add Carrousel

---
+++ src/components/Carrousel/index.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react"
+import { ChevronLeft, ChevronRight } from "react-bootstrap-icons"
+
+function Carrousel(props) {
+    const title = props.title // récupération du titre
+    const link = props.link // récupération de url des img
+
+    let left // chevron left
+    let right // chevron right
+
+    // useState pour changment des img
+    const [url, changeUrl] = useState(link[0])
+
+    // SI il y a plus de 1 img -> afficher les flêches
+    if (link.length > 1) {
+        left = (
+            <ChevronLeft
+                className="carrousel__icon left"
+                onClick={() => beforeImg()}
+            />
+        )
+        right = (
+            <ChevronRight
+                className="carrousel__icon right"
+                onClick={() => nextImg()}
+            />
+        )
+    }
+
+    // Event pour img suivante
+    function nextImg() {
+        let newLink
+
+        // SI dernière url -> revenir à la première img
+        link.indexOf(url) < link.length - 1
+            ? (newLink = link[link.indexOf(url) + 1])
+            : (newLink = link[0])
+        changeUrl(newLink)
+    }
+
+    // Event pour img précédente
+    function beforeImg() {
+        let newLink
+
+        // SI première url -> aller à la dernière img
+        link.indexOf(url) > 0
+            ? (newLink = link[link.indexOf(url) - 1])
+            : (newLink = link[link.length - 1])
+        changeUrl(newLink)
+    }
+
+    return (
+        <div className="carrousel">
+            {left}
+            <img className="carrousel__img" src={url} alt={title} />
+            {right}
+        </div>
+    )
+}
+
+export default Carrousel
