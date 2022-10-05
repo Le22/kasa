f6c2bd68a82f1dc0f6699fea479b1f8c260586d4
Louis-Étienne Girard <contact@louisetiennegirard.fr>
2022-10-05 08:08:49 +0200

    Add Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <img
+        className="footer__img"
+        src="./media/logo-white.png"
+        alt="Logo Kasa"
+        width={122}
+      />
+      <p className="footer__copyright">© 2020 Kasa. All rights reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
